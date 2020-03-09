bd4cf53d21572cd62de88b10db1e2f12061a1136
Josepereira08 <jpyftsunami08@gmail.com>
2020-03-08 21:50:29 -0430

    Practica_Vue-07

--- js/app.js
+++ js/app.js
@@ -1,66 +1,81 @@
 
-var vm = new Vue({
-                el: '#app',
-                data:{
-                	draft: '',
-                	new_task:'',
+Vue.component('app-icon', {
+    template: '<span :class="cssClasses" aria-hidden="true"></span>',
+    props: ['img'],
+    computed: {
+        cssClasses: function () {
+            return 'glyphicon glyphicon-'+this.img;
+        }
+    }
+});
 
-                	tasks:[
-	                	{
-	                		description:'Aprender vue',
-	                		pending: true,
-	                		editing:false
-	                	},
-	                	{
-	                		description:'Aprender saas',
-	                		pending: true, 
-	                		editing:false
-	                	},
-	                	{
-	                		description:'Aprender php',
-	                		pending: false,
-	                		editing:false
-	                	}
-	                ]
-                },
-                methods: {
-			        createTask: function () {
-			            this.tasks.push({
-			                description: this.new_task,
-			                pending: true,
-			                editing: false
-			            });
-
-			            this.new_task = '';
-			        },
-			        toggleStatus: function (task) {
-			            task.pending = !task.pending;
-			        },
-			        editTask: function (task) {
-			            this.tasks.forEach(function (task) {
-			                task.editing = false;
-			            });
-
-			            this.draft = task.description;
-
-			            task.editing = true;
-			        },
-			        updateTask: function (task) {
-			            task.description = this.draft;
-
-			            task.editing = false;
-			        },
-			        discardTask: function (task) {
-			            task.editing = false;
-			        },
-			        deleteTask: function (index) {
-			            this.tasks.splice(index, 1);
-			        },
-			        deleteCompleted: function () {
-			            this.tasks = this.tasks.filter(function (task) {
-			                return task.pending;
-			            });
-			        }
-
-    			}
+Vue.component('app-task', {
+    template: '#task-template',
+    props: ['tasks', 'task', 'index'],
+    methods: {
+        toggleStatus: function () {
+            this.task.pending = !this.task.pending;
+        },
+       edit: function () {
+            this.tasks.forEach(function (task) {
+                task.editing = false;
             });
+
+            this.draft = this.task.description;
+
+            this.task.editing = true;
+        },
+        update: function () {
+            this.task.description = this.draft;
+
+            this.task.editing = false;
+        },
+        discard: function () {
+            this.task.editing = false;
+        },
+        remove: function () {
+            this.tasks.splice(this.index, 1);
+        },
+    }
+});
+
+var vm = new Vue({
+    el: '#app',
+    methods: {
+        createTask: function () {
+            this.tasks.push({
+                description: this.new_task,
+                pending: true,
+                editing: false
+            });
+
+            this.new_task = '';
+        },
+        deleteCompleted: function () {
+            this.tasks = this.tasks.filter(function (task) {
+                return task.pending;
+            });
+        }
+    },
+    data: {
+        draft: '',
+        new_task: '',
+        tasks:[
+	        {
+	        	description:'Aprender vue',
+	        	pending: true,
+	        	editing:false
+	        },
+	        {
+	        	description:'Aprender saas',
+	        	pending: true, 
+	        	editing:false
+	        },
+	        {
+	        	description:'Aprender php',
+	        	pending: false,
+	        	editing:false
+	        }
+	    ]
+    }
+});
